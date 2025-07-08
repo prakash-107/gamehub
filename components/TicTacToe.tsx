118db9f803b7143df93fc18531b65a057a4909f6
prakash-107 <prakahgilbert234@gmail.com>
2025-07-09 01:22:17 +0500

    fix ESlint error:use const in winner loop

--- components/TicTacToe.tsx
+++ components/TicTacToe.tsx
@@ -116,7 +116,7 @@ function calculateWinner(squares: string[]) {
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6],
   ];
-  for (let [a, b, c] of lines) {
+  for (const [a, b, c] of lines) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
